cae343e14e17741c12b2fde89340fe096b87c9d2
iooosef <josence22@gmail.com>
2025-04-27 23:32:45 +0800

    Refactor main method to run Spring application and display current directory

--- backend/src/main/java/tip/Main.java
+++ backend/src/main/java/tip/Main.java
@@ -1,7 +1,16 @@
 package tip;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        SpringApplication.run(Main.class, args);
+        System.out.println("Current directory: " + System.getProperty("user.dir"));
     }
+
+
 }
