8890de59437e5bcc69ead0d781231e4b162cc71b
iooosef <josence22@gmail.com>
2025-04-28 20:21:39 +0800

    Added Springboot Security to Java

---
+++ backend/src/main/java/tip/config/SecurityConfiguration.java
@@ -0,0 +1,66 @@
+package tip.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+
+/*
+ * SecurityConfiguration
+ * - to configure the security settings for the application
+ * - Springboot Spring Security configuration
+ */
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    // Defines a bean for authentication manager to be used in the application
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            // CORS configuration
+            .cors(cors ->
+                    cors.configurationSource(request
+                            -> new CorsConfiguration().applyPermitDefaultValues()))
+            // Disable CSRF for LAN-only access
+            .csrf(csrf ->
+                    csrf.disable())
+            .authorizeHttpRequests(authz -> authz
+                    .requestMatchers("/",
+                        "/index.html",
+                        "/static/**",
+                        "/login",
+                        "/register",
+                        "/auth/**",
+                        "/me",
+                        "/user/roles",
+                        "/config.json").permitAll()
+                    .requestMatchers("/admin/**").hasRole("ADMIN")
+                    .requestMatchers("/level-editor/**").hasAnyRole("LEVEL-EDITOR", "ADMIN")
+                    .anyRequest().authenticated()
+            )
+            .formLogin(form -> form
+                    .loginPage("/")
+                    .permitAll()
+            )
+            .logout(logout -> logout
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                    .logoutSuccessUrl("/")
+                    .permitAll()
+            );
+        return http.build();
+    }
+}
