cb7d295c32131dad25f91c103ea3985fa4d710a1
iooosef <josence22@gmail.com>
2025-05-04 17:39:05 +0800

    Added roboto-condensed font

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import MiniMap from "./components/MiniMap";
+import Login from "./Login";
+import Register from "./Register";
+import MainMenu from "./MainMenu";
+import '@fontsource-variable/roboto-condensed';
 
 export default function App() {
   return (
-    <div className="w-screen h-screen p-4 relative">
-      <MiniMap />
+    <div className="w-screen h-screen">
+      <MainMenu />
     </div>
   );
 }
