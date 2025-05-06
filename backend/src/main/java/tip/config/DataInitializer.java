f8549b2ff7bf1401659d308a371c7cc8eedd1f7f
iooosef <josence22@gmail.com>
2025-05-06 11:48:16 +0800

    Added data initialization for country and country adjacent and the sql files involved

--- backend/src/main/java/tip/config/DataInitializer.java
+++ backend/src/main/java/tip/config/DataInitializer.java
@@ -1,12 +1,18 @@
 package tip.config;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.core.JdbcTemplate;
 import tip.model.Gender;
 import tip.model.User;
+import tip.repository.CountryAdjacentRepository;
 import tip.repository.CountryRepository;
 import tip.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.nio.file.Files;
 
 /*
  * DataInitializer
@@ -17,18 +23,38 @@ public class DataInitializer implements CommandLineRunner {
 
     private final UserRepository userRepository;
     private final CountryRepository countryRepository;
+    private final CountryAdjacentRepository countryAdjacentRepository;
     private final BCryptPasswordEncoder passwordEncoder;
+    private final JdbcTemplate jdbcTemplate;
 
-    public DataInitializer(UserRepository userRepository, CountryRepository countryRepository, PasswordEncoder passwordEncoder) {
+    public DataInitializer(UserRepository userRepository,
+                           CountryRepository countryRepository,
+                           CountryAdjacentRepository countryAdjacentRepository,
+                           PasswordEncoder passwordEncoder,
+                           JdbcTemplate jdbcTemplate) {
         this.userRepository = userRepository;
         this.countryRepository = countryRepository;
+        this.countryAdjacentRepository = countryAdjacentRepository;
         this.passwordEncoder = (BCryptPasswordEncoder) passwordEncoder;
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     @Override
     public void run(String... args) throws Exception {
         // Check if the user table is empty
+        if (countryRepository.count() == 0) {
+            Resource resource = new ClassPathResource("insert-countries.sql");
+            String sql = new String(Files.readAllBytes(resource.getFile().toPath()));
+            jdbcTemplate.execute(sql);
+            System.out.println("Inserted countries");
+        }
 
+        if (countryAdjacentRepository.count() == 0) {
+            Resource resource = new ClassPathResource("country_adjacent.sql");
+            String sql = new String(Files.readAllBytes(resource.getFile().toPath()));
+            jdbcTemplate.execute(sql);
+            System.out.println("Inserted country adjacents");
+        }
 
         if (userRepository.count() == 0) {
             // Create an admin user
