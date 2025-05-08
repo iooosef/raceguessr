fe73f290b1f8215723450e825859b33914bac5be
iooosef <josence22@gmail.com>
2025-05-08 20:26:34 +0800

    Add LevelController with endpoints for retrieving tags and quick play subjects

--- backend/src/main/java/tip/controller/LevelController.java
+++ backend/src/main/java/tip/controller/LevelController.java
@@ -1,12 +1,22 @@
 package tip.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tip.model.User;
+import tip.model.dto.SubjectDifficulty;
 import tip.repository.SubjectRepository;
 import tip.repository.TagRepository;
+import tip.repository.UserRepository;
+import tip.service.SortService;
+import tip.service.SubjectService;
+
+import java.util.Comparator;
+import java.util.List;
 
 @RestController
 @RequestMapping("/levels")
@@ -15,9 +25,27 @@ public class LevelController {
     private TagRepository tagRepository;
     @Autowired
     SubjectRepository subjectRepository;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    SubjectService subjectService;
+    @Autowired
+    SortService sortService;
 
     @RequestMapping(value = "/data/tags", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> getAllTags() {
         return ResponseEntity.ok(tagRepository.findAll());
     }
+
+    @RequestMapping(value = "/quickplay", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getQuickPlay(Sort sort) {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userRepository.findByEmail(email).get();
+        int user_Id = user.getId();
+
+        List<SubjectDifficulty> quickplayRounds = subjectService.getTenSubjectIDs((long) user_Id);
+        sortService.mergeSort(quickplayRounds, Comparator.comparingInt(SubjectDifficulty::getDifficulty));
+        return ResponseEntity.ok(quickplayRounds);
+    }
+
 }
