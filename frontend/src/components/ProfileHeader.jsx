47a0b92b1b853ee292f5048acd5b7c0f3b786197
iooosef <josence22@gmail.com>
2025-05-08 17:26:47 +0800

    Refactor ProfileHeader component to accept headerText prop and improve user loading logic

--- frontend/src/components/ProfileHeader.jsx
+++ frontend/src/components/ProfileHeader.jsx
@@ -1,22 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import { useUser } from '../auth/UserContext';
 
-const ProfileHeader = () => {
+import SVGLoadHorizontal from './SVGLoadHorizontal'
+
+const ProfileHeader = ({headerText}) => {
     const { user, loading } = useUser();
     const [isUserLoaded, setIsUserLoaded] = useState(false);
     
     useEffect(() => {
-        if (user || loading === false) {
+        if (!loading && user) {
             setIsUserLoaded(true);
         }
     }, [user, loading]);
 
     return (
-        <div id='user-info-container' className='px-4 py-2 flex items-center gap-5 italic font-bold rounded-2xl
-            transition-all duration-300 ease-in-out transform hover:scale-105'
-            style={{background: '#2c3e50', fontFamily: 'Roboto Condensed Variable', fontSize: '1.2em'}}>
-            <span className='text-white'>{user.displayName}</span>
-            <span style={{color: '#f1c40f'}}>Score: 00000</span>
+        <div className='flex font-guessr gap-8'>
+            <div id='user-info-container' className='px-4 py-2 flex items-center gap-5 italic font-bold rounded-2xl
+                transition-all duration-300 ease-in-out transform hover:scale-105'
+                style={{background: '#2c3e50', fontSize: '1.2em'}}>
+                <span className='text-white'>
+                    {user.displayName
+                        ?? <SVGLoadHorizontal className='text-white' />}
+                </span>
+                <span style={{color: '#f1c40f'}}>Score: 00000</span>
+            </div>
+            <div className='flex items-center'>
+                <h1 className='text-white text-4xl'>{headerText}</h1>
+            </div>
         </div>
     );
 }
