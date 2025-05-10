3096e48e118fb6223b561f3628b0861373691b66
iooosef <josence22@gmail.com>
2025-05-11 06:25:18 +0800

    Fixed getTenSubjectIDs returning nothing

--- backend/src/main/java/tip/service/SubjectService.java
+++ backend/src/main/java/tip/service/SubjectService.java
@@ -17,11 +17,11 @@ public class SubjectService {
         this.subjectRepository = subjectRepository;
     }
 
-    public List<SubjectDifficulty> getTenSubjectIDs(Long user_Id) {
+    public List<SubjectDifficulty> getTenSubjectIDs(Integer user_Id) {
         List<Subject> undone = subjectRepository
             .findRandomSubjectsUnansweredBy(user_Id, PageRequest.of(0, 10));
         int remaining = 10 - undone.size();
-        if (remaining > 10) {
+        if (remaining > 0) {
             List<Subject> done = subjectRepository
                     .findRandomSubjectsAnsweredBy(user_Id, PageRequest.of(0, remaining));
             undone.addAll(done);
